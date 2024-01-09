489c9091e9ba3bae8421f8cb1e512b8179ab72fa
Don Benny <donbenny916@gmail.com>
2024-01-09 14:56:15 +0530

    new commit optimized

--- application.py
+++ application.py
@@ -1,25 +1,13 @@
 from flask import Flask, jsonify, request
 import cv2
 import pytesseract
-import numpy as np  
-import tensorflow as tf 
+import numpy as np
+import tensorflow as tf
 import requests
 import re
 from io import BytesIO
 import json
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-#augment your_application
-datagen = ImageDataGenerator(
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    vertical_flip=True,
-    fill_mode='nearest'
-)
 
 # Set Tesseract path
 pytesseract.pytesseract.tesseract_cmd = 'pytesseract/tesseract.exe'
@@ -32,18 +20,15 @@ model = tf.keras.models.load_model("pan_detector_model_v7.h5")
 def preprocess_image_from_url(image_url):
     response = requests.get(image_url)
     img = cv2.imdecode(np.frombuffer(response.content, np.uint8), cv2.IMREAD_COLOR)
-    # Apply augmentation
-    # img = datagen.random_transform(img)
     img = cv2.resize(img, (224, 224))
     img = img / 255.0
     img = np.float32(img)
     img = np.expand_dims(img, axis=0)
     return img
+
 def extract_information(img):
     r = re.compile(r"([A-Z]+\s[A-Z]+\s[A-Z]+)|([A-Z]{5}[0-9]{4}[A-Z]{1})|([0-9]{2}/[0-9]{2}/[0-9]{4})")
     margin = 10
-    # augmented_image_path = 'path_to_save_augmented_image.jpg'
-    # cv2.imwrite(augmented_image_path, img)
     # Convert the image to grayscale
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
@@ -69,7 +54,7 @@ def extract_information(img):
     for (id, text) in bag:
         if "Name" in text:
             name_region = img[data['top'][id]+data['height'][id]:data['top'][id]+2*data['height'][id], 
-                          data['left'][id]:data['left'][id]+2*data['width'][id]]
+                               data['left'][id]:data['left'][id]+2*data['width'][id]]
     if 'name_region' in locals():
         name_gray = cv2.cvtColor(name_region, cv2.COLOR_BGR2GRAY)
         name_thresh = cv2.threshold(name_gray, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)[1]
@@ -98,23 +83,17 @@ def extract_information(img):
 def predictByImageFromURL(image_url):
     # Preprocess the input image
     input_image = preprocess_image_from_url(image_url)
-    
+
     # Run the inference
     predictions = model.predict(input_image)
     predicted_class_index = np.argmax(predictions)
+
     class_labels = ["PAN", "Others"]
-    
     predicted_class = class_labels[predicted_class_index]
-    
+
     if predicted_class == "PAN":
         response = requests.get(image_url)
-     
         img = cv2.imdecode(np.frombuffer(response.content, np.uint8), cv2.IMREAD_COLOR)
-
-        # Apply augmentation
-        # img = datagen.random_transform(img)
-        # augmented_image_path = 'path_to_save_augmented_image.jpg'
-        # cv2.imwrite(augmented_image_path, img)
 
         # Extract information using regular expressions
         info = extract_information(img)
